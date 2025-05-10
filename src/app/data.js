ce72d135700bd460e7164de1e12dcc0f38b91fd0
SayuruDev <mailtosayuru@gmail.com>
2025-05-10 13:38:37 -0400

    Home Component

---
+++ src/app/data.js
@@ -0,0 +1,139 @@
+// this comtains the data we are gonna for AboutMe component and slider component
+
+export const AboutData = [
+  {
+    count: 1,
+    title: "Frontend Developer",
+    img: "/first.svg",
+    array: [
+      "Tailwind CSS",
+      "Javascript (ES6)",
+      "React 18 ",
+      "Bootstrap",
+      "HTML 5.0 / CSS 3.0",
+      "Javascript Frameworks",
+      "Figma - Wireframes",
+    ],
+  },
+  {
+    count: 2,
+    title: "Back-end Developer",
+    img: "/second.svg",
+    array: [
+      "Node.js",
+      "Express JS",
+      "Rest API",
+      "MongoDB",
+      "MongoDB Atles",
+      "Middleware",
+      "Routing",
+    ],
+  },
+  {
+    count: 3,
+    title: "Full Stack Developer",
+    img: "/third.svg",
+    array: [
+      "React JS / Next.js",
+      "Node  / Express / MongoDB",
+      "Routing, Redux  ",
+      "MiddleWare,Authentication,JWT",
+      "Node Packages",
+      "Tailwind CSS",
+      "Admin Dashboard",
+    ],
+  },
+  {
+    count: 4,
+    title: "Python Developer",
+    img: "/fourth.svg",
+    array: [
+      "Python Syntax & Libraries",
+      "Object-Oriented Programming (OOP)",
+      "Database Interaction",
+      "Python Libraries",
+      "NumPy",
+      "Pandas",
+      "PyCharm / Jupyter Notebook",
+      "Google Colab",
+    ],
+  },
+  {
+    count: 5,
+    title: "CMS and Headless CMS",
+    img: "/fifth.svg",
+    array: [
+      "Contentful",
+      "GraphCMS",
+      "Strapi",
+      "WordPress (Gutenberg)",
+      "Directus",
+      "Sanity",
+      "SiteBaan (My Own)",
+    ],
+  },
+  {
+    count: 6,
+    title: "Tools and Things",
+    img: "/sisxth.svg",
+    array: [
+      "VSCode",
+      "Git / Github",
+      "Bash / Powershell",
+      "NPM / Yarn",
+      "Plesk Control Panel",
+      "Vercel / Stormkit / Netlify",
+      "Postman / Insomnia",
+    ],
+  },
+];
+
+// this is for the slider
+export const DataArray = [
+  {
+    title: "Ultimate Nextjs13 Portfolio Website",
+    name: "Nextjs13 Portfolio Website",
+    des: "I have made this project in 2023  to show thousands of students all over the world  how to make Reactjs/Nextjs portfolio. A portfolio website that will enable you to showcase your projects to your client or your employer The design of this portfolio was taken from a friend. It was carefully and professionally design to work in all devices",
+    des1: "This project was made using, Ractjs, Nextjs, NodeMailer  and many more!",
+    images: ["/portfolioimage.png", "/nextportfolio1.png"],
+  },
+  {
+    title: "Personal Portfolio Website Using Reactjs",
+    name: "Ultimate ReactJs portfolio",
+    des: "I have made this project in 2022  to show thousands of students all over the world  how to make Reactjs portfolio.",
+    des1: "This project was made using, Ractjs, Nextjs, Expressjs, Nodejs, Rxjs Bootsrap and many more!",
+    images: ["/reactportfolio.png"],
+  },
+  {
+    title: "Management Consulting Company Web Design",
+    name: "edubaba Management Consultants",
+    des: "We buit this project for edubaba  Management Consultants.",
+    des1: "",
+
+    images: ["/hotel01.png"],
+  },
+  {
+    title: "Real Estate and Property Management Project",
+    name: "Inans Property Management ",
+    des: "We built this project for inans group LTD",
+    des1: "Aside from its gorgeous design, this website is backed by an outstanding plugin that allows owners and agencies to simply display different properties in the neighbourhood and create an online showcase in an appealing manner for home buyers and tenants..",
+
+    images: ["/property1.png"],
+  },
+  {
+    title: "Crypto Trading Website",
+    name: "Crypto Trading project",
+    des: "We buit this website for BigMoney group",
+    des1: "This project was built to enable clients all over the world to buy and sell their crypto.",
+
+    images: ["/crypto5.png", "/crypto3.png", "/crypto1.png"],
+  },
+  {
+    title: "Food Delivery Company WebSite",
+    name: "Yum Food Company",
+    des: "We did  this project for Yum Foods Located in USA.",
+    des1: "This website's graphic design is fantastic. Animated sliders are painstakingly built and created in WordPress with Revolution Slider. All of the product photos are photo manipulations created in Adobe Photoshop.",
+
+    images: ["/onboarding.jpg", "/yumfood.jpg", "/deliveryguy.png"],
+  },
+];
--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,100 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+import HomeComponent from "./components/HomeComponent/HomeComponent";
+import { Hind } from "next/font/google";
+import MySlider from "./components/SliderCard/MySlider";
+import AboutMe from "./components/AboutMe/AboutMe";
+import Testimonial from "./components/Testimonial/Testimonial";
+
+
+
+
+const hind = Hind({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export default function Home() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      setIsScrolled(scrollTop > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="min-h-screen relative">
+      <HomeComponent />   
+      <div
+        className="mt-0 pt-5 -mb-40"
+        id="portfolio"
+        style={{
+          backgroundImage:
+          "linear-gradient(-62deg, #EEF7FB 0 50%,  white 0% 100%)",
+          width: "100%",
+        }}
+      >
+         <div className="container m-auto">
+          <p
+            className="text-[300px]  text-[#F7FBFD] md:pl-[50px] px-5 max-w-[750px] w-[100%] overflow-hidden"
+            style={{ transform: "translate(0px,-20px)" }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Portfolio
+          </p>
+          <div className="" style={{ transform: "translate(0px, -290px)" }}>
+            <p className="text-[#48AFDE]  md:pl-[80px] px-5 font-extrabold text-5xl">
+              Recent Works
+            </p>
+            <p
+              className={`max-w-2xl md:pl-[80px] px-5 font-[300] text-[16px] text-[#47626D] leading-8 mt-5 ${hind.className}`}
+            >
+              Here are a few of my most recent work. As a web designer and
+              full-stack web developer, I constantly prioritise 100% client
+              satisfaction. I always enjoy working on my projects, so each one
+              is a new adventure for me. While working on each new project, I
+              attempt to learn new things. As a result, my paintings are always
+              current and one-of-a-kind.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div 
+        style={{ transform: "translate(0px,-230px)" }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn1
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <MySlider/>
+        </div>
+        </div>
+        <AboutMe/>
+      <Testimonial/>
+
+      {isScrolled ? (
+        <div
+          onClick={() => window.scrollTo(0, 0)}
+          className="bg-red-500 p-4 w-fit rounded-xl fixed bottom-8 right-8 cursor-pointer z-50 animate-bounce"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={5}
+            stroke="currentColor"
+            className="w-6 h-6 text-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 15.75l7.5-7.5 7.5 7.5"
+            />
+          </svg>
+        </div>
+      ) : (
+        ""
+      )}
+    </main>
   );
 }
